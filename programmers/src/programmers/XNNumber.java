540446eb5058e7e611aa43482f2f09963ba79ea6
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-05-18 22:55:49 +0900

    05.18

---
+++ programmers/src/programmers/XNNumber.java
@@ -0,0 +1,20 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class XNNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int x = sc.nextInt();
+		int n = sc.nextInt();		
+	   
+	}
+
+	public static long[] solution(int x, int n) {
+		long[] answer = {};
+        return answer;
+	}
+
+}
